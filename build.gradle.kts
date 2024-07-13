bbb58338bf64920e11e377705d721a2e59d975f0
fvlaenix <fvlaenixf@gmail.com>
2024-07-13 15:12:47 +0400

    Add Github CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.google.protobuf.gradle.id
 
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.google.protobuf") version "0.9.4"
 }
 
@@ -45,6 +47,25 @@ task<JavaExec>("runServer") {
     classpath = sourceSets.main.get().runtimeClasspath
     mainClass.set("com.fvlaenix.RunServerKt")
 }
+
+fun createJarTaskByJavaExec(name: String) = tasks.create<ShadowJar>("${name}Jar") {
+    mergeServiceFiles()
+    group = "shadow"
+    description = "Run server $name"
+
+    from(sourceSets.main.get().output)
+    from(project.configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+    configurations = listOf(project.configurations.runtimeClasspath.get())
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    archiveFileName.set("${name}.jar")
+    manifest {
+        attributes["Main-Class"] = (tasks.findByName(name) as JavaExec).mainClass.get()
+    }
+}.apply task@ { tasks.named("jar") { dependsOn(this@task) } }
+
+createJarTaskByJavaExec("runServer")
 
 protobuf {
     protoc {
