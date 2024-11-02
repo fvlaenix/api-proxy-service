8af81c0693b6fcb45bdd654397148c92c6dcc1e0
fvlaenix <fvlaenixf@gmail.com>
2024-11-02 19:07:10 +0400

    Increase message size for server

--- src/main/kotlin/ProxyServer.kt
+++ src/main/kotlin/ProxyServer.kt
@@ -1,14 +1,16 @@
 package com.fvlaenix
 
+import com.fvlaenix.ChannelUtils.STANDARD_IMAGE_CHANNEL_SIZE
 import com.fvlaenix.database.DatabaseConfiguration
 import io.grpc.Server
 import io.grpc.ServerBuilder
 import io.grpc.ServerInterceptors
 
 class ProxyServer(port: Int, databaseConfiguration: DatabaseConfiguration) {
-  private val server: Server = ServerBuilder.forPort(port).addService(
-    ServerInterceptors.intercept(ProxyService(databaseConfiguration), ProxyApiInterceptor())
-  ).build()
+  private val server: Server = ServerBuilder.forPort(port)
+    .addService(ServerInterceptors.intercept(ProxyService(databaseConfiguration), ProxyApiInterceptor()))
+    .maxInboundMessageSize(STANDARD_IMAGE_CHANNEL_SIZE)
+    .build()
 
   fun start() {
     server.start()
